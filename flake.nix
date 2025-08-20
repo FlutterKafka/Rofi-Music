820b53b10ef040bafc579aeade64005bdca913c4
Kafka <207027844+FlutterKafka@users.noreply.github.com>
2025-08-20 21:02:55 +0200

    Testing flake inputs

--- flake.nix
+++ flake.nix
@@ -3,41 +3,47 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rofi-music-src = {
+      url = "github:FlutterKafka/Rofi-Music";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "aarch64-darwin";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      packages.${system}.default = pkgs.stdenv.mkDerivation {
-        name = "rofi-music";
-        src = self;
+  outputs = { self, nixpkgs, flake-utils, rofi-music-src }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "rofi-music";
+          src = rofi-music-src;
 
-        buildInputs = with pkgs; [
-          mpv
-          choose-gui
-          terminal-notifier
-        ];
+          buildInputs = with pkgs; [
+            mpv
+            choose-gui
+            terminal-notifier
+          ];
 
-        installPhase = ''
-          mkdir -p $out/bin
-          cp rofi-music $out/bin/
-          chmod +x $out/bin/rofi-music
-        '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp rofi-music $out/bin/
+            chmod +x $out/bin/rofi-music
+          '';
 
-        meta = with pkgs.lib; {
-          description = "Bash script using rofi (choose-gui) to play local music on macOS";
-          homepage = "https://github.com/FlutterKafka/Rofi-Music";
-          license = licenses.mit;
-          maintainers = [ "FlutterKafka" ];
+          meta = with pkgs.lib; {
+            description = "Bash script using rofi (choose-gui) to play local music on macOS";
+            homepage = "https://github.com/FlutterKafka/Rofi-Music";
+            license = licenses.mit;
+            maintainers = [ "FlutterKafka" ];
+          };
         };
-      };
 
-      apps.${system}.default = {
-        type = "app";
-        program = "${self.packages.${system}.default}/bin/rofi-music";
-      };
-    };
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/rofi-music";
+        };
+      }
+    );
 }
